232bff0178374415b8bc3acfac0afd6092fda25e
Nirvana.icy <jinglong.bi@me.com>
2016-03-29 14:37:16 +0800

    Update tinyPng.py
    support copy content.json file

--- tinyPng.py
+++ tinyPng.py
@@ -1,24 +1,29 @@
 import tinify
 import os
 import os.path
+import shutil
 
-tinify.key = "xxxx"  #App Key
+tinify.key = "xxxx"
 fromFilePath = "xxxx"
 toFilePath = "xxxx"
 
 for root, dirs, files in os.walk(fromFilePath):
-	for name in files:
-		fileName, fileSuffix = os.path.splitext(name)
-		if fileSuffix == '.png' or fileSuffix == '.jpg':
-			toFullPath = toFilePath + root[len(fromFilePath):]
-			toFullName = toFullPath + '/' + name
+    for name in files:
+        fileName, fileSuffix = os.path.splitext(name)
+        toFullPath = toFilePath + root[len(fromFilePath):]
+        toFullName = toFullPath + '/' + name
+        if not os.path.exists(toFullPath):
+            os.makedirs(toFullPath)
+        if fileSuffix == '.png' or fileSuffix == '.jpg':
 
-			if not os.path.exists(toFullPath):
-        		   os.makedirs(toFullPath)
-
-			source = tinify.from_file(root + '/' + name)
-			source.to_file(toFullName)
-
-			with open(toFullName, 'rb') as source:
-			    source_data = source.read()
-			    result_data = tinify.from_buffer(source_data).to_buffer()
+                source = tinify.from_file(root + '/' + name)
+                source.to_file(toFullName)
+                        
+                with open(toFullName, 'rb') as source:
+                    source_data = source.read()
+                    result_data = tinify.from_buffer(source_data).to_buffer()
+        else:
+            if fileSuffix == '.json':
+                shutil.copy2(root + '/' + name, toFullName)
+            else:
+                print ("copied failed %s"%(root + '/' + name))
